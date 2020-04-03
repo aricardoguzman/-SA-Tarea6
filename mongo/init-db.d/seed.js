c70876569b24345e3ab96728ffb4b36fd79d1bed
aricardoguzman <201010425@ingenieria.usac.edu.gt>
2020-04-03 00:54:28 -0600

    tarea 6

---
+++ mongo/init-db.d/seed.js
@@ -0,0 +1,29 @@
+
+db.createCollection('notas')
+db.notas.insertMany([
+  {
+    carnet: 201010425,
+    name: 'Tarea 1',
+    nota: 100
+  },
+  {
+    carnet: 201010425,
+    name: 'Tarea 2',
+    nota: 100
+  },
+  {
+    carnet: 201010425,
+    name: 'Tarea 3',
+    nota: 100
+  },
+  {
+    carnet: 201010425,
+    name: 'Tarea 4',
+    nota: 100
+  },
+  {
+    carnet: 201010425,
+    name: 'Tarea 5',
+    nota: 100
+  }
+])
